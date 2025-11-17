5f5f0d0e207bea8542f0822712483d8ded2f31a2
lulicorbo <lucia.corbo2@gmail.com>
2025-11-17 19:10:51 -0300

    core_8

--- core_formularios/src/components/Card.jsx
+++ core_formularios/src/components/Card.jsx
@@ -3,11 +3,11 @@ import styles from '../css/Card.module.css';
 const Card = ({ nombre, apellido }) => {
     return (
         
-            <div className={styles.card}>
-            <h3 className={styles.title}>
-                {nombre} {apellido}
-            </h3>
-            </div>
+        <div className={styles.card}>
+        <h3 className={styles.title}>
+            {nombre} {apellido}
+        </h3>
+        </div>
         
     );
 };
